a3e5915df85f0a808fb7600b5cb10f02fe919c5d
matt bimpson <m.bimpson@gmail.com>
2020-05-23 13:15:19 -0500

    form styling

--- src/components/CreateAccount/CreateAccountForm.tsx
+++ src/components/CreateAccount/CreateAccountForm.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import { FormContainer, InputText } from './Styles';
+import { FormContainer, InputText, FormItem } from './Styles';
 
 const CreateAccountForm: React.FC<any> = () => {
   return (
     <>
       <FormContainer>
         <h2>Create Account</h2>
-        <p>First name: </p>
-        <InputText></InputText>
+        <FormItem>
+          <p>First name: </p>
+          <InputText type="text"></InputText>
+        </FormItem>
+        <FormItem>
+          <p>Last name:</p>
+          <InputText type="text"></InputText>
+        </FormItem>
+        <FormItem>
+          <p>Phone:</p>
+          { /* todo: move the type into styled component props */ }
+          <InputText type="tel"></InputText>
+        </FormItem>
       </FormContainer>
     </>
   )
--- src/components/CreateAccount/Styles.ts
+++ src/components/CreateAccount/Styles.ts
@@ -4,13 +4,23 @@ export const FormContainer =
 	styled.div`
         margin: 10px;
         padding: 15px;
-		background-color: white;
-	`;
+        background-color: white;
+        width: 50%;
+
+        @media (max-height: 500px) {
+            width: fit-content;
+        }
+    `;
 
 export const InputText =
-    styled.input.attrs({ type: 'text'})`
+    styled.input`
         border-radius: 5px;
         height: 20px;
         width: 200px;
         padding: 2px;
+    `;
+
+export const FormItem =
+    styled.div`
+        margin-bottom: 20px;
     `;
